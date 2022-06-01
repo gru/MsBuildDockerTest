ec84de56ae321d2e7f2545e6c75d19f981f45806
Андрюшин Андрей Александрович <andrey.andryushin@stoloto.ru>
2022-06-01 18:47:28 +0300

    Добавил пример для MSBuild в linux.

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+﻿FROM mcr.microsoft.com/dotnet/sdk:6.0 AS base
+WORKDIR /app
+
+FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build
+WORKDIR /src
+COPY ["MsBuildDockerTest.csproj", "./"]
+RUN dotnet restore "MsBuildDockerTest.csproj"
+COPY . .
+WORKDIR "/src/"
+RUN dotnet build "MsBuildDockerTest.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "MsBuildDockerTest.csproj" -c Release -o /app/publish
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "MsBuildDockerTest.dll"]
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Build.Evaluation;
+using Microsoft.Build.Locator;
+
+namespace MsBuildDockerTest
+{
+    public class Program
+    {
+        static Program()
+        {
+            MSBuildLocator.RegisterDefaults();
+        }
+    
+        public static void Main()
+        {
+            var sample = new MsBuildSample();
+            var value = sample.EvaluateHelloWorldProperty();
+
+            Console.WriteLine(value);
+        }
+    }
+
+    public class MsBuildSample
+    {
+        public string EvaluateHelloWorldProperty()
+        {
+            var project = new Project(@"MsBuildDockerTest.csproj");
+            var property = project.Properties
+                .Single(p => p.Name == "HelloWorld");
+
+            return property.EvaluatedValue;
+        }
+    }
+}
